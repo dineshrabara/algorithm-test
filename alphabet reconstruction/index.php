c11809aa19c0b1512c10af04b1971573c1ec90a3
Dinesh Rabara <dinesh.rabara@gmail.com>
2020-05-03 14:00:56 +0530

    add alphabet reconstruction test

---
+++ alphabet reconstruction/index.php
@@ -0,0 +1,38 @@
+<?php //php 7.0.8
+
+class Solution
+{
+    /**
+     * @param String[] $words
+     * @return String[]
+     */
+
+    public function reconstruct_alphabet($words)
+    {
+        $result = [];
+        // Put your code here
+        $lengths = max(array_map('strlen', $words)) - 1;
+        for ($i = 0; $i < $lengths; $i++) {
+
+            foreach ($words as $key => $word) {
+                if (empty($word[$i]) || in_array($word[$i], $result)) {
+                    continue;
+                }
+                $result[] = $word[$i];
+            }
+        }
+
+        // Return the result, don't change the structure
+        return $result;
+    }
+}
+
+$words = ['ccda', 'ccb', 'cd', 'a', 'ab', 'd'];
+
+while (($line = readline()) !== "" && !empty($line)) {
+    array_push($words, $line);
+}
+
+$solution = new Solution();
+$result = $solution->reconstruct_alphabet($words);
+print implode("", $result);
